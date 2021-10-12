454ba1107ff2ab9835b31668e5dee577dcc776a4
Abdulmalik <abumuhab98@gmail.com>
2021-10-12 09:37:15 +0100

    env config

--- index.js
+++ index.js
@@ -59,10 +59,12 @@ app.use((err, req, res, next) => {
   })
 })
 
-server.listen(config.port, '192.168.43.91', () => {
-  console.log(`Server running on port ${config.port}`)
-})
-
-// server.listen(config.port, () => {
-//   console.log(`Server running on port ${config.port}`)
-// })
+if (config.env === 'prod') {
+  server.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`)
+  })
+} else {
+  server.listen(config.port, '192.168.43.91', () => {
+    console.log(`Server running on port ${config.port}`)
+  })
+}
