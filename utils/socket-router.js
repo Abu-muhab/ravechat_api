411d04d5fc704d5412c778c508c77e439d70e0cb
Abdulmalik <abumuhab98@gmail.com>
2021-10-18 20:34:10 +0100

    welcome message init

--- utils/socket-router.js
+++ utils/socket-router.js
@@ -1,3 +1,6 @@
+const { userServiceInstance } = require('../services/index')
+const chatEventBus = require('../eventbus/chat')
+
 class SocketRouter {
   use (path, middlewares, onConnect) {
     this.path = path
@@ -10,8 +13,25 @@ class SocketRouter {
       io.of(this.path).use(middleware)
     })
 
-    io.of(this.path).on('connection', (socket) => {
-      console.log('New connection')
+    io.of(this.path).on('connection', async (socket) => {
+      const raveAdmin = await userServiceInstance.findUserByUserName('@abdulmalik')
+      const user = await userServiceInstance.findUserByUserName(socket.userName)
+
+      if (raveAdmin !== undefined && user !== undefined) {
+        // send welcome message of new user
+        chatEventBus.next({
+          type: 'new-chat',
+          senderDetails: raveAdmin,
+          targets: [socket.userName],
+          message: {
+            content: 'Welcome to rave chat',
+            from: raveAdmin.userName,
+            to: socket.userName,
+            time: new Date().toISOString()
+          }
+        })
+      }
+
       this.onConnect(socket)
     })
   };
